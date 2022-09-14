d747ce4a4958b01833ea295a14fd6247b0e84125
Swarzstein <zorrillagja@gmail.com>
2022-09-14 17:19:13 -0400

    Reviewer fixes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,5 @@ function displayMenu() {
   document.querySelector('#menu-button').classList.toggle('hidden');
   document.querySelector('#wide-menu').classList.toggle('hidden');
 }
-
 document.querySelector('#menu-button').addEventListener('click', displayMenu);
 document.querySelector('#wide-menu > div > button').addEventListener('click', displayMenu);
-document.querySelector('#wide-menu > div > a').addEventListener('click', displayMenu);
