28c6f55638321261ebc0a88811a001a158a9e2b3
Swarzstein <zorrillagja@gmail.com>
2022-09-14 13:30:13 -0400

    added onclick events to the html from the js file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-function displayMenu(){
+function displayMenu() {
   document.querySelector('#menu-button').classList.toggle('hidden');
   document.querySelector('#wide-menu').classList.toggle('hidden');
 }
+
+document.querySelector('#menu-button').addEventListener('click', displayMenu);
+document.querySelector('#wide-menu > div > button').addEventListener('click', displayMenu);
+document.querySelector('#wide-menu > div > a').addEventListener('click', displayMenu);
